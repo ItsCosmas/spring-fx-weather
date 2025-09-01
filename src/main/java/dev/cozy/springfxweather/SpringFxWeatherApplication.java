1aef88f8459017229b038e8bb543082718828d22
Cosmas Gikunju <25123254+ItsCosmas@users.noreply.github.com>
2025-09-01 12:53:42 +0300

    disable webserver

--- src/main/java/dev/cozy/springfxweather/SpringFxWeatherApplication.java
+++ src/main/java/dev/cozy/springfxweather/SpringFxWeatherApplication.java
@@ -1,5 +1,6 @@
 package dev.cozy.springfxweather;
 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,6 +10,7 @@ public class SpringFxWeatherApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new SpringApplicationBuilder(SpringFxWeatherApplication.class)
+                .web(WebApplicationType.NONE) // Disables web server
                 .headless(false) // allows GUI
                 .run(args);
 
